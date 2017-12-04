e5b511f4643bda8d5f7c404a0393dad5d3679623
Daniel <danthehillman@gmail.com>
2017-12-03 23:26:18 -0600

    E Indicator support, New 'Run Animation'

---
+++ Assets/Scripts/UI and Menu Control/InteractIndicator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+
+public class InteractIndicator : MonoBehaviour
+{
+    Image interactIcon;
+    bool iconOn;
+    bool overrideOff;
+
+    void Start()
+    {
+        iconOn = false;
+        overrideOff = false;
+    }
+
+    void Update()
+    {
+        if(iconOn)
+        {
+            if(Input.GetButtonDown("Grab"))
+            {
+                interactIcon.gameObject.SetActive(false);
+                iconOn = false;
+            }
+        }
+    }
+     
+	void OnTriggerEnter(Collider other)
+    {
+        if(!overrideOff)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                overrideOff = true;
+                interactIcon.gameObject.SetActive(true);
+                iconOn = true;
+            }
+        }
+    }
+}
