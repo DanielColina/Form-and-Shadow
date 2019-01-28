13a718712ee6e8b03991659ac925e1772171633b
DanielColina <danthehillman@gmail.com>
2019-01-27 20:40:01 -0600

    Very, Very Early Character Work
    -Explored early iteration of character state sytem using System.Flags and bitwise operators.

---
+++ Assets/Scripts/Gameplay/Player/PlayerMotor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    [System.Flags]
+    public enum PlayerState
+    {
+        None = 0,
+        Grounded = 1 << 0,
+        Airborne = 1 << 1,
+        Dead = 1 << 2,
+    }
+    public PlayerState currentState;
+
+    public void SetState(PlayerState state)
+    {
+        currentState |= state;
+    }
+
+    public void UnsetState(PlayerState state)
+    {
+        currentState &= state;
+    } 
+
+    public bool CheckState(PlayerState state)
+    {
+        return (currentState & state) == state;
+    }
+
+    private void Update()
+    {
+        
+    }
+}
